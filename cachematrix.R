8b8d34735d6f4c48685327a4f88984590c345c8e
unknown <g.sharp.minor@Gmail.com>
2014-05-18 18:29:26 -0700

    first update

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,18 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Creates a matrix "object" with embedded functions to interact with it
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(the_matrix = matrix()) {
+  cached_inverse <- NULL;
+  set <- function(y){
+    the_matrix <<- y;
+    cached_inverse <<- NULL;
+  }
+  get <- function() the_matrix;
+  setInverse <- function(input_inverse) cached_inverse <- input_inverse;
+  getInverse <- function() cached_inverse;
+  list(set=set, get=get, setInverse=setInverse, getInverse=getInverse);
 }
 
 
